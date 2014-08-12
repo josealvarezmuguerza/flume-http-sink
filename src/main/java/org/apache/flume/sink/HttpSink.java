e4b64d964a377c7253b278d4927ddf515fcef132
josealvarezmuguerza <josealvarezmuguerza@gmail.com>
2014-08-12 01:15:34 -0300

    emit REST calls with json payloads

--- src/main/java/org/apache/flume/sink/HttpSink.java
+++ src/main/java/org/apache/flume/sink/HttpSink.java
@@ -3,11 +3,12 @@ package org.apache.flume.sink;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.util.StringTokenizer;
+import java.net.UnknownHostException;
 
 import org.apache.flume.Channel;
 import org.apache.flume.Context;
@@ -17,6 +18,8 @@ import org.apache.flume.Transaction;
 import org.apache.flume.conf.Configurable;
 import org.apache.log4j.Logger;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * @author jose alvarez muguerza
@@ -25,6 +28,19 @@ public class HttpSink extends AbstractSink implements Configurable {
 	private String wsURL;
 
 	private static final Logger LOG = Logger.getLogger(HttpSink.class);
+	private static final JsonParser JSON_PARSER = new JsonParser();
+	private static String HOST ;
+	private static final String ARRAY_START = "[";
+	private static final String ARRAY_END = "]";
+	
+	static {
+		try {
+			HOST = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public void configure(Context context) {
 		String wsEndpoint = context.getString("endpoint", "127.0.0.1");
@@ -54,7 +70,7 @@ public class HttpSink extends AbstractSink implements Configurable {
 
 	public Status process() throws EventDeliveryException {
 		Status status = null;
-		
+
 		// Start transaction
 		Channel ch = getChannel();
 		Transaction txn = ch.getTransaction();
@@ -64,16 +80,18 @@ public class HttpSink extends AbstractSink implements Configurable {
 			String message = URLEncoder.encode(new String(event.getBody()),
 					"UTF-8");
 			URL serverURL = new URL(this.wsURL);
-			LOG.debug("New event ready: " + message + " --> " + wsURL);
-			
+			char[] jsonMessage = convertToJSON(message);
+			LOG.debug("New event ready: " + new String(jsonMessage) + " --> "
+					+ wsURL);
+
 			HttpURLConnection connection = (HttpURLConnection) serverURL
 					.openConnection();
 			connection.setDoOutput(true);
 			connection.setRequestMethod("POST");
 			OutputStreamWriter wr = new OutputStreamWriter(
 					connection.getOutputStream());
-			
-			wr.write(message);
+
+			wr.write(jsonMessage);
 			wr.flush();
 			connection.getInputStream();
 			connection.disconnect();
@@ -97,23 +115,15 @@ public class HttpSink extends AbstractSink implements Configurable {
 		return status;
 	}
 
-	//private char[] convertToJSON(final String message) {
-	//	JSONObject jsonObj = new JSONObject();
-		
-	//	StringTokenizer tokens = new StringTokenizer(message, " - ");
-	//	while (tokens.hasMoreElements()){
-	//		dataSet.put("id", ) ;
-	//		dataSet.put("client_id", "SomeValueB") ;
-	//		dataSet.put("keyC", "SomeValueC") ;	
-	//	}
-		
-		
-	//	return jsonObj.append("news", news);
-		
-	//	JSONObject dataSet = new JSONObject();
-		
-		
-	//}
+	private char[] convertToJSON(final String message) {
+		JsonObject jsonObject = (JsonObject)JSON_PARSER.parse(
+				"{\"headers\" : {\"timestamp\" : \""
+						+ System.currentTimeMillis() + "\",\"host\" : \""
+						+ HOST + "\"},\"body\" : \"" + message + "\" }");
+		String result =  ARRAY_START + jsonObject.toString()+ ARRAY_END;
+		return result.toCharArray() ;
+
+	}
 
 	private void validateFullEndpoint(final String endpoint, final String port) {
 		try {
@@ -121,13 +131,16 @@ public class HttpSink extends AbstractSink implements Configurable {
 			URLConnection conn = url.openConnection();
 			conn.connect();
 		} catch (MalformedURLException e) {
-			String errMsg = "Web Service endpoint is malformed: (" + endpoint + ":" + port + ")";
+			String errMsg = "Web Service endpoint is malformed: (" + endpoint
+					+ ":" + port + ")";
 			LOG.error(errMsg);
-			new EventDeliveryException(errMsg );
+			new EventDeliveryException(errMsg);
 		} catch (IOException e) {
-			String errMsg = "Web Service endpoint is not valid: (" + endpoint + ":" + port + ")";
+			String errMsg = "Web Service endpoint is not valid: (" + endpoint
+					+ ":" + port + ")";
 			LOG.error(errMsg);
 			new IOException(errMsg);
 		}
 	}
+
 }
